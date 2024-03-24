bb279c54244285da835c3449f58831feafe91c10
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-03-24 16:30:10 +0300

    24.03 - bug - 2

---
+++ src/components/logIn/LogIn.stories.tsx
@@ -0,0 +1,23 @@
+import type {Meta, StoryObj} from '@storybook/react'
+import {LogIn} from './LogIn'
+import {HashRouterDecorator, ReduxStoreProviderDecorator} from '../../stories/decorators/ReduxStoreProviderDecorator'
+
+
+const meta: Meta<typeof LogIn> = {
+    title: 'TODOLISTS/LogIn',
+    component: LogIn,
+
+    tags: ['autodocs'],
+     decorators: [ReduxStoreProviderDecorator, HashRouterDecorator]
+
+}
+
+export default meta
+
+type Story = StoryObj<typeof LogIn>;
+
+
+export const LogInStoryExample =()=> {
+    return <LogIn/>
+}
+
