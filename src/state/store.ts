204c82534dbcf4055083308bb022dea8ae7dd366
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-04-01 15:14:36 +0300

    01.04 - 23 lesson - 1

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,27 +1,28 @@
-import { combineReducers } from "redux";
-import { tasksReducer } from "./reducers/tasks-reducer";
-import { toDoListsReducer } from "./reducers/todolists-reducer";
-import { thunk } from "redux-thunk";
-import { useDispatch } from "react-redux";
-import { appReducer } from "./reducers/app-reducer";
-import { authReducer } from "./reducers/auth-reducer";
-import { configureStore } from "@reduxjs/toolkit";
+import {combineReducers} from 'redux'
+import {tasksReducer} from './reducers/tasks-reducer'
+import {toDoListsReducer} from './reducers/todolists-reducer'
+import {thunk} from 'redux-thunk'
+import {useDispatch} from 'react-redux'
+import {appReducer} from './reducers/app-reducer'
+import {authReducer} from './reducers/auth-reducer'
+import {configureStore} from '@reduxjs/toolkit'
 
 const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  toDoLists: toDoListsReducer,
-  app: appReducer,
-  auth: authReducer
-});
+    tasks: tasksReducer,
+    toDoLists: toDoListsReducer,
+    app: appReducer,
+    auth: authReducer
+})
 
 /* Создали RTK store */
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk)
-});
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk)
+})
 
 // Типизация rootReducer
 export type RootReducerType = typeof rootReducer;
+
 
 // Типизация всего STATE
 export type AppRootStateType = ReturnType<typeof store.getState>;
@@ -30,4 +31,4 @@ export type AppRootStateType = ReturnType<typeof store.getState>;
 // Типизация dispatch по RTK
 export type AppDispatch = typeof store.dispatch;
 // Самопальный useDispatch
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch = () => useDispatch<AppDispatch>()
