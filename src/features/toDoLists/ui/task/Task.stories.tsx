b9d73675eed8e391cfa44241e01d0a4211c571db
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-04-08 15:17:12 +0300

    08.04 - Lesson 20 - 3

--- src/features/toDoLists/ui/task/Task.stories.tsx
+++ src/features/toDoLists/ui/task/Task.stories.tsx
@@ -1,52 +1,48 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { Task } from "./Task";
-import { TasksPriorities, TasksStatuses } from "../../../../utils/api/enums";
+import type {Meta, StoryObj} from '@storybook/react'
+import {Task} from './Task'
+import {TasksPriorities, TasksStatuses} from '../../../../utils/api/enums'
 
 const meta: Meta<typeof Task> = {
-  title: "TODOLISTS/Task",
-  component: Task,
+    title: 'TODOLISTS/Task',
+    component: Task,
 
-  tags: ["autodocs"],
+    tags: ['autodocs'],
 
-  args: {
-    onChangeStatusHandler: action("Status changed inside Task"),
-    changeTaskTitle: action("Title changed inside Task"),
-    onClickRemoveHandler: action("Remove Button clicked changed inside Task"),
-    task: {
-      id: "12wsdewfijdei234asd3",
-      title: "JS",
-      status: TasksStatuses.New,
-      priority: TasksPriorities.Low,
-      deadline: "",
-      order: 0,
-      startDate: "",
-      description: "Hello",
-      addedDate: "",
-      todoListId: "TodolistId",
-    },
-  },
-};
+    args: {
+        task: {
+            id: '12wsdewfijdei234asd3',
+            title: 'JS',
+            status: TasksStatuses.New,
+            priority: TasksPriorities.Low,
+            deadline: '',
+            order: 0,
+            startDate: '',
+            description: 'Hello',
+            addedDate: '',
+            todoListId: 'TodolistId'
+        }
+    }
+}
 
-export default meta;
+export default meta
 
 type Story = StoryObj<typeof Task>;
 
-export const TaskIsNotDoneStory: Story = {};
+export const TaskIsNotDoneStory: Story = {}
 
 export const TaskIsDoneStory: Story = {
-  args: {
-    task: {
-      id: "12wsdewfijdei2343",
-      title: "CSS",
-      status: TasksStatuses.Completed,
-      priority: TasksPriorities.Low,
-      deadline: "",
-      order: 0,
-      startDate: "",
-      description: "Hello",
-      addedDate: "",
-      todoListId: "TodolistId",
-    },
-  },
-};
+    args: {
+        task: {
+            id: '12wsdewfijdei2343',
+            title: 'CSS',
+            status: TasksStatuses.Completed,
+            priority: TasksPriorities.Low,
+            deadline: '',
+            order: 0,
+            startDate: '',
+            description: 'Hello',
+            addedDate: '',
+            todoListId: 'TodolistId'
+        }
+    }
+}
--- src/features/toDoLists/ui/task/Task.tsx
+++ src/features/toDoLists/ui/task/Task.tsx
@@ -1,58 +1,77 @@
-import React, { ChangeEvent, memo, useCallback } from "react";
-import S from "./Task.module.css";
-import Checkbox from "@mui/material/Checkbox";
-import { EditableSpan } from "../../../../components/editableSpan/EditableSpan";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { TasksType } from "features/toDoLists/api/tasks-api";
-import { RequestStatusType } from "app/model/app-reducer";
-import { TasksStatuses } from "utils/api/enums";
+import React, {ChangeEvent, memo} from 'react'
+import S from './Task.module.css'
+import Checkbox from '@mui/material/Checkbox'
+import {EditableSpan} from '../../../../components/editableSpan/EditableSpan'
+import IconButton from '@mui/material/IconButton'
+import DeleteIcon from '@mui/icons-material/Delete'
+import {TasksType} from 'features/toDoLists/api/tasks-api'
+import {RequestStatusType} from 'app/model/app-reducer'
+import {TasksStatuses} from 'utils/api/enums'
+import {useActions} from '../../../../utils/hooks/useActions'
+import {tasksThunks} from '../../model/tasks/tasks-reducer'
 
 type TaskPropsType = {
-  task: TasksType;
-  onChangeStatusHandler: (taskId: string, status: TasksStatuses) => void;
-  changeTaskTitle: (newTitle: string) => void;
-  onClickRemoveHandler: (id: string) => void;
-  entityStatus: RequestStatusType;
+    task: TasksType;
+    entityStatus: RequestStatusType;
+    toDoListID: string
 };
 
 export const Task: React.FC<TaskPropsType> = memo(
-  ({ task, onChangeStatusHandler, changeTaskTitle, onClickRemoveHandler, entityStatus }) => {
-    // -------------- Работа с изменением status ----------------
-    const onChangeHandler = useCallback(
-      (e: ChangeEvent<HTMLInputElement>) => {
-        let isDone = e.currentTarget.checked ? TasksStatuses.Completed : TasksStatuses.New;
+    ({task, entityStatus, toDoListID}) => {
 
-        onChangeStatusHandler(task.id, isDone);
-      }, [task.id]);
+        // Используя useAction получили callbacks в которые уже входит dispatch
+        const {deleteTaskTC, updateTaskStatusTC, updateTaskTitleTC} = useActions(tasksThunks)
 
-    return (
-      <div
-        className={task.status === TasksStatuses.Completed
-          ? `${S.to_Do_List__list} ${S.is_done}`
-          : S.to_Do_List__list}
-      >
-        <div className={S.to_Do_List__list_box}>
-          <Checkbox
-            color={"success"}
-            checked={task.status === TasksStatuses.Completed}
-            onChange={onChangeHandler}
-            disabled={entityStatus === "loading"}
-          />
 
-          <EditableSpan value={task.title} onChange={(newTitle) => changeTaskTitle(newTitle)}
-                        disabled={entityStatus} />
-        </div>
+        // -------------- Изменяем Task status ----------------
+        const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
+            const status =
+                e.currentTarget.checked ? TasksStatuses.Completed : TasksStatuses.New
 
-        <IconButton
-          aria-label="delete"
-          size="small"
-          onClick={() => onClickRemoveHandler(task.id)}
-          disabled={entityStatus === "loading"}
-        >
-          <DeleteIcon fontSize="small" />
-        </IconButton>
-      </div>
-    );
-  }
-);
+            updateTaskStatusTC({toDoListID, taskId: task.id, status})
+        }
+
+
+        // -------------- Удаление task ----------------
+        const removeTaskHandler = () => {
+            deleteTaskTC({toDoListID, taskId: task.id})
+        }
+
+
+        // -------------- Меняем Task's title ----------------
+        const changeTaskTitleHandler = (newTitle: string) => {
+            updateTaskTitleTC({toDoListID, taskId: task.id, title: newTitle})
+        }
+
+
+        return (
+            <div
+                className={task.status === TasksStatuses.Completed
+                    ? `${S.to_Do_List__list} ${S.is_done}`
+                    : S.to_Do_List__list}
+            >
+                <div className={S.to_Do_List__list_box}>
+                    <Checkbox
+                        color={'success'}
+                        checked={task.status === TasksStatuses.Completed}
+                        onChange={changeTaskStatusHandler}
+                        disabled={entityStatus === 'loading'}
+                    />
+
+                    <EditableSpan value={task.title}
+                                  onChange={(newTitle) => changeTaskTitleHandler(newTitle)}
+                                  disabled={entityStatus}/>
+                </div>
+
+                <IconButton
+                    aria-label="delete"
+                    size="small"
+                    onClick={removeTaskHandler}
+                    disabled={entityStatus === 'loading'}
+                >
+                    <DeleteIcon fontSize="small"/>
+                </IconButton>
+            </div>
+        )
+    }
+)
