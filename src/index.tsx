f434f3cb8e6f392c1fd28f515a858796408c9f35
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-08-22 08:01:40 +0300

    22.08 - hash router - 2

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,13 @@ import { HashRouter } from "react-router-dom";
 const rerenderEntireTree = () => {
   const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
   root.render(
-    <Provider store={store}>
-      <HashRouter>
+    <HashRouter>
+      <Provider store={store}>
+
         <App />
-      </HashRouter>
-    </Provider>
+
+      </Provider>
+    </HashRouter>
   );
 };
 
